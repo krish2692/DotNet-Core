29d09e856d2f12e1a3e11a959e6c9a8192a5826d
Krishna Surti <krishsurti2692@gmail.com>
2022-08-15 19:00:25 -0500

    Commit before adding the rpg game service

--- SampleOne-ChangeTheme/app.js
+++ SampleOne-ChangeTheme/app.js
@@ -16,3 +16,60 @@ switcher.addEventListener('click', function(){
 
     console.log('current class name: ' + className);
 });
+
+
+
+const logs = [
+    { 'user': '1', 'page': 'A'},
+    { 'user': '2', 'page': 'B'},
+    { 'user': '1', 'page': 'B'},
+    { 'user': '1', 'page': 'D'},
+    { 'user': '2', 'page': 'A'},
+    { 'user': '3', 'page': 'B'},
+    { 'user': '3', 'page': 'D'},
+    { 'user': '1', 'page': 'C'},
+    { 'user': '3', 'page': 'C'},
+    { 'user': '1', 'page': 'C'},
+    { 'user': '2', 'page': 'C'},
+    { 'user': '3', 'page': 'B'},
+    { 'user': '1', 'page': 'A'},
+    { 'user': '3', 'page': 'C'},
+]
+
+function getHistoryByUser(logs) {
+    const h = {}
+    for (let l of logs) {
+        if (h[l.user]) {
+            h[l.user].push(l.page)
+        } else {
+            h[l.user] = [l.page]
+        }
+    }
+    return h
+}
+
+function getHistoryCombinations(history) {
+    return Object.keys(history).reduce((acc, user) => {
+        if (history[user].length < 3) return acc
+        
+        for(let i = 0; i < history[user].length - 3; i++) {
+            const combination = history[user].slice(i, i+3).join('')
+            
+            acc[combination] = (acc[combination] || 0) + 1
+            
+            if (acc[combination] > acc.maxValue) {
+                acc.max = combination
+                acc.maxValue = acc[combination]
+            }
+        }
+        
+        return acc
+    }, { max: "", maxValue: 0 })
+}
+
+function getMaxCombination(historyCombinations) {
+    return historyCombinations.max
+}
+
+const mostPopularSequence = [getHistoryByUser, getHistoryCombinations, getMaxCombination].reduce((res, fn) => fn(res), logs)
+console.log(mostPopularSequence);
--- WebApi/Controllers/PizzaController.cs
+++ WebApi/Controllers/PizzaController.cs
@@ -15,7 +15,7 @@ public class PizzaController : ControllerBase
     // GET all action
     [HttpGet]
     public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
-
+    
     // GET by Id action
     [HttpGet("{id}")]
     public ActionResult<Pizza> Get(int id)
@@ -66,4 +66,6 @@ public class PizzaController : ControllerBase
         PizzaService.Delete(id);
         return NoContent();
     }
+
+    
 }
